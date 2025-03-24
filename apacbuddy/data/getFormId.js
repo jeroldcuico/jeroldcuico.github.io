54f04830c2544440a9688058b68c9312d73cf074
jeroldcuico <loverold17@gmail.com>
2025-03-25 01:41:08 +0800

    Added if Form is Version 3

--- apacbuddy/data/getFormId.js
+++ apacbuddy/data/getFormId.js
@@ -1,6 +1,6 @@
 export function getFormId() {
     const formContainer = document.querySelector(".fsform-container");
-    const dataid = formContainer ? formContainer.getAttribute("data-formid") : null;
+    const dataid = formContainer ? formContainer.getAttribute("data-formid") ?? document.querySelector(".fsForm > input[name=form]").value : null;
     const formError = document.getElementById("error") || document.querySelector(".formError"); // If workflow is unpublished
     const isFormDisabled = document.querySelector('[class*="StyledErrorBanner"]'); // If form is unpublished and disabled
     const [product , formlinkName] = location.pathname.split("/").filter(Boolean)
@@ -18,7 +18,7 @@ export function getFormId() {
     return {
       formid: dataid,
       isErrorForm: !!(formError || isFormDisabled),
-      version: version,
+      version: version ?? document.querySelector(".fsForm > input[name=style_version]").value,
       product: product,
       formPathname: `${product}/${formlinkName}`,
       formName: formName,
